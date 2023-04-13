d0d9415376bbc44e9ae5dfc2355d7b6de0023d0c
likalikabaz <89534939+likalikabaz@users.noreply.github.com>
2023-04-13 16:35:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+import statsmodels.stats.weightstats as w
+
+chat_id = 753540521  # Ваш chat ID, не меняйте название переменной
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+def solution(x: np.array, y: np.array) -> bool:  # Одна или две выборке на входе, заполняется исходя из условия
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    _, pvalue = w.ztest(x, value=500, alternative='larger')
+
+    return pvalue <= 0.05
